ca162d8fddb30172df492627b112aca45ab363ef
Gerardo Gordin <gerardogordin@gmail.com>
2022-09-27 19:20:02 -0300

    Pre-Entrega1

--- recursos/simulador.js
+++ recursos/simulador.js
@@ -4,6 +4,7 @@ let edad = parseInt(prompt('Ingrese su edad'))
 while (isNaN(edad)){
   edad=parseInt (prompt('Debes ingresar valores numericos'))
 } 
+confirm('Hola '+nombreUsuario+' Estas seguro de tener '+edad+ ' años?')
 
 if (edad < 18){
   alert('No puedes ingresar. Este sitio es para personas mayores de 18 años')
