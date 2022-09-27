e17d495a1057af164c9b3dd80f523a32b4740504
Gerardo Gordin <gerardogordin@gmail.com>
2022-09-26 23:07:05 -0300

    preentrega1/js

---
+++ recursos/simulador.js
@@ -0,0 +1,19 @@
+let nombreUsuario = prompt('Ingresá tu nombre')
+let edad = parseInt(prompt('Ingrese su edad'))
+
+while (isNaN(edad)){
+  edad=parseInt (prompt('Debes ingresar valores numericos'))
+} 
+
+if (edad < 18){
+  alert('No puedes ingresar. Este sitio es para personas mayores de 18 años')
+  document.write ('Por su seguridad, le pedimos que no vuelva a ingresar y mienta su edad.')
+}
+else{
+  alert('Bienvenida/o. Puedes ingresar')
+  document.write ('Este sitio es seguro.')
+}
+
+
+
+
